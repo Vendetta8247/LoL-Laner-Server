173388d1979b7ec2ce03ca43bd1325010324e5e7
Vendetta8247 <vendetta8247@gmail.com>
2016-12-25 02:54:34 +0200

    First commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get("/summoner/by-name/:name", function(req, res)
 	});
 })
 
-var server = app.listen(8081, function () {
+var server = app.listen(process.env.PORT || 5000, function () {
 
   var host = server.address().address
   var port = server.address().port
